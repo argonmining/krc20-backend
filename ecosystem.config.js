12094945ed5c0b6c363e25a483eba673a643a6a8
argonmining <ashton@onargon.com>
2024-09-07 00:43:10 +0200

    gitignore

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,6 +4,7 @@ module.exports = {
             name: 'krc20-backend',
             script: 'dist/app.js',
             instances: 1,
+            exec_mode: 'fork',
             autorestart: true,
             watch: false,
             max_memory_restart: '1G',
