978637c8ff6db489b3c6d6295a5ac4d79a7bffd1
CrixusHD <nick.engl@gmx.de>
2024-12-10 18:55:45 +0100

    bugfixes-and-improvements-v1
    announcements

--- src/routes/apiRoutes/announcements.ts
+++ src/routes/apiRoutes/announcements.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, {Request, Response} from 'express';
 import {PrismaClient} from '@prisma/client';
 import multer from "multer";
 import logger from "../../utils/logger";
@@ -15,16 +15,14 @@ router.get('/all', async (req, res) => {
     res.json({result: announcements})
 })
 // Set up multer for file uploads
-const uploadDir = process.env.FILESYSTEMDIR + '/announcements' || '/var/www/announcements'; // Provide a default value
+const uploadDir = (process.env.FILESYSTEMDIR ?? '/var/www') + '/announcements'; // Provide a default value
 
 const storage = multer.diskStorage({
     destination: (req: Request, file: Express.Multer.File, cb: (error: Error | null, destination: string) => void) => {
         cb(null, uploadDir);
     },
     filename: (req: Request, file: Express.Multer.File, cb: (error: Error | null, filename: string) => void) => {
-        const ticker = req.params.ticker; // Get the ticker from the URL
-        const extension = path.extname(file.originalname); // Preserve the original file extension
-        cb(null, `${ticker}${extension}`);
+        cb(null, file.originalname);
     },
 });
 
@@ -33,17 +31,20 @@ const upload = multer({storage});
 // Endpoint to upload a new logo and update the database
 router.post('/create', upload.single('image'), async (req: Request, res: Response) => {
     try {
+        if (req.body?.title === undefined){
+            res.status(500).json({message: 'No Title',});
+        }
 
         const announcement = await prisma.announcements.create({
             data: {
-                title: req.title,
-                text: req.text,
+                title: req.body.title,
+                text: req.body.text,
                 timestamp: new Date(),
             },
         });
         if (req.file){
             // Construct the logo URL
-            const announcementUrl = `/announcements/${req.file?.filename}`;
+            const announcementUrl = `/announcements/${req.file?.originalname}`;
             await prisma.announcements.update({
                 where: {id: announcement.id},
                 data: {imageUrl: announcementUrl}
@@ -57,6 +58,5 @@ router.post('/create', upload.single('image'), async (req: Request, res: Respons
         res.status(500).json({error: 'Internal server error'});
     }
 });
-router.post('/create')
 
 module.exports = router
