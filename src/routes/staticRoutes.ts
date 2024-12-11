da497fe0f9d839605556e5a5c0129eb0be2cded9
CrixusHD <nick.engl@gmx.de>
2024-12-11 10:57:31 +0100

    bugfixes-and-improvements-v1
    announcements

--- src/routes/staticRoutes.ts
+++ src/routes/staticRoutes.ts
@@ -1,6 +1,7 @@
 import express, {Request, Response} from 'express';
 import path from "path";
 import * as fs from "fs";
+import logger from "../utils/logger";
 
 const router = express.Router()
 const filepath = process.env.FILESYSTEMDIR || '/var/www';
@@ -10,8 +11,10 @@ const loadFile = (req: Request, res: Response, contentPath: string) => {
     const pathToFile = path.join(filepath, contentPath, filename)
 
     if (fs.existsSync(pathToFile)) {
+        logger.info(pathToFile + " exists")
         return res.sendFile(pathToFile)
     }
+    logger.info(pathToFile + " not exists")
     return res.status(404).json({error: 'Content not found'})
 }
 
