d9922d8fe887659646896fc1371c2b8250efeb11
CrixusHD <nick.engl@gmx.de>
2024-12-11 18:19:58 +0100

    bugfixes-and-improvements-v1
    announcements

--- src/routes/staticRoutes.ts
+++ src/routes/staticRoutes.ts
@@ -7,6 +7,19 @@ const router = express.Router()
 // const filepath = process.env.FILESYSTEMDIR || '\\var\\www';
 const filepath = '/var/www';
 
+
+router.get('/logos/:filename', async (req: Request, res: Response) => loadFile(req, res, '/krc20-logos'))
+router.get('/announcements/:filename', (req, res, next) => {
+    console.log({
+        'req:': req,
+        'res': res
+    })
+    next()
+}, async (req: Request, res: Response) => loadFile(req, res, '/announcements'))
+
+router.use('/logos', express.static(path.join(filepath, '/krc20-logos')))
+router.use('/announcements', express.static(path.join(filepath, '/announcements')))
+
 const loadFile = (req: Request, res: Response, contentPath: string) => {
     const {filename} = req.params
     const pathToFile = `${filepath}${contentPath}/${filename}`
@@ -19,17 +32,4 @@ const loadFile = (req: Request, res: Response, contentPath: string) => {
     logger.info(pathToFile + " not exists")
     return res.status(404).json({error: 'Content not found'})
 }
-
-router.use('/logos', express.static(path.join(filepath, '\\krc20-logos')))
-router.use('/announcements', express.static(path.join(filepath, '\\announcements')))
-
-router.get('/logos/:filename', (req, res, next) => {
-    console.log({
-        'req:': req,
-        'res': res
-    })
-    next()
-}, async (req: Request, res: Response) => loadFile(req, res, '/krc20-logos'))
-router.get('/announcements/:filename', async (req: Request, res: Response) => loadFile(req, res, '/announcements'))
-
 module.exports = router
