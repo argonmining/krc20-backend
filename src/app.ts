7c20d213e5699a41ee3f31c21bead7b4b0b48374
CrixusHD <nick.engl@gmx.de>
2024-12-12 18:57:23 +0100

    bugfixes-and-improvements-v1
    announcements without cors

--- src/app.ts
+++ src/app.ts
@@ -24,7 +24,7 @@ const corsOptions = {
 };
 
 // Use CORS middleware with options
-// app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 
 // Ensure CORS is applied before static files
