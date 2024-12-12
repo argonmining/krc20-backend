18f0ebabe6e15248dd415c9d23ac1736f685a72e
CrixusHD <nick.engl@gmx.de>
2024-12-12 19:38:17 +0100

    bugfixes-and-improvements-v1
    announcements middle

--- src/app.ts
+++ src/app.ts
@@ -32,7 +32,7 @@ app.use(function (req, res, next) {
 });
 
 // Use the routers
-app.use('/static', staticRouter);
+app.use('/data', staticRouter);
 app.use('/api', apiRouter);
 
 app.get('/health', (req, res) => {
