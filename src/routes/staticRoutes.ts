d4cf1cf35a82d1aa5f6d45930d21e7250e1eb3ca
CrixusHD <nick.engl@gmx.de>
2024-12-13 16:04:15 +0100

    bugfixes-and-improvements-v1
    announcements middle

--- src/routes/staticRoutes.ts
+++ src/routes/staticRoutes.ts
@@ -7,44 +7,40 @@ const router = express.Router()
 // const filepath = process.env.FILESYSTEMDIR || '\\var\\www';
 const filepath = '/var/www/static';
 
-router.use('/logos', (req, res, next) => {
-    logger.warn('logo')
-    logger.warn({
-        'req:': req,
-        'res': res
-    })
-    next()
-}, express.static(path.join(filepath, '/krc20-logos')))
-router.use('/announcements', (req, res, next) => {
-    logger.warn('announce')
-    logger.warn({
-        'req:': req,
-        'res': res
-    })
-    next()
-}, express.static(path.join(filepath, '/announcements')))
-
-// router.get('/logos/:filename', async (req: Request, res: Response) => loadFile(req, res, '/krc20-logos'))
-// router.get('/announcements/:filename', (req: Request, res: Response, next) => {
-//     logger.warn('announce')
+// router.use('/logos', (req, res, next) => {
+//     logger.warn('logo')
+//     logger.warn({
+//         'req:': req,
+//         'res': res
+//     })
 //     next()
-// }, async (req: Request, res: Response) => {
-//     logger.warn("announcement request")
-//     res.json("ok");
-// })
+// }, express.static(path.join(filepath, '/krc20-logos')))
+// router.use('/announcements', (req, res, next) => {
+//     logger.warn('announce')
+//     logger.warn({
+//         'req:': req,
+//         'res': res
+//     })
+//     next()
+// }, express.static(path.join(filepath, '/announcements')))
 
-// loadFile(req, res, '/announcements')
+router.get('/logos/:filename', async (req: Request, res: Response) => loadFile(req, res, '/krc20-logos'))
+router.get('/announcements/:filename', (req: Request, res: Response, next) => {
+    logger.warn('announce')
+    next()
+}, async (req: Request, res: Response) => loadFile(req, res, '/announcements'))
 
-// const loadFile = (req: Request, res: Response, contentPath: string) => {
-//     const {filename} = req.params
-//     const pathToFile = `${filepath}${contentPath}/${filename}`
-//     logger.error(pathToFile)
-//
-//     if (fs.existsSync(pathToFile)) {
-//         logger.info(pathToFile + " exists")
-//         return res.sendFile(pathToFile)
-//     }
-//     logger.info(pathToFile + " not exists")
-//     return res.status(404).json({error: 'Content not found'})
-// }
+
+const loadFile = (req: Request, res: Response, contentPath: string) => {
+    const {filename} = req.params
+    const pathToFile = `${filepath}${contentPath}/${filename}`
+    logger.error(pathToFile)
+
+    if (fs.existsSync(pathToFile)) {
+        logger.info(pathToFile + " exists")
+        return res.sendFile(pathToFile)
+    }
+    logger.info(pathToFile + " not exists")
+    return res.status(404).json({error: 'Content not found'})
+}
 module.exports = router
