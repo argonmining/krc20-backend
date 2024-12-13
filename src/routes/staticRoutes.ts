ed99cf8eb495a19d15d4e4195ade539e0c5280af
CrixusHD <nick.engl@gmx.de>
2024-12-13 17:42:40 +0100

    bugfixes-and-improvements-v1
    announcements cors

--- src/routes/staticRoutes.ts
+++ src/routes/staticRoutes.ts
@@ -1,39 +1,28 @@
 import express, {Request, Response} from 'express';
 import * as fs from "fs";
 import logger from "../utils/logger";
-import path from "path";
 
 const router = express.Router()
 const filesystemDir = process.env.FILESYSTEMDIR || '/var/www';
 const filepath = filesystemDir + '/static';
 
 router.get('/logos/:filename', async (req: Request, res: Response) => loadFile(req, res, '/krc20-logos'))
-router.get('/announcements/:filename', (req: Request, res: Response, next) => {
+router.get('/announcements/:filename/:extension', (req: Request, res: Response, next) => {
     logger.warn('announce')
     next()
 }, async (req: Request, res: Response) => loadFile(req, res, '/announcements'))
 
 
 const loadFile = (req: Request, res: Response, contentPath: string) => {
-    const {filename} = req.params
-    const pathToFile = `${filepath}${contentPath}/${filename}`
+    const {filename, extension} = req.params
+    const pathToFile = `${filepath}${contentPath}/${filename}.${extension}`
     logger.warn(pathToFile)
     logger.warn(fs.existsSync(pathToFile))
 
     if (fs.existsSync(pathToFile)) {
         logger.info(pathToFile + " exists")
 
-        const options = {
-            root: `${filepath}${contentPath}`
-        };
-
-        res.sendFile(filename, options, function (err) {
-            if (err) {
-                console.error('Error sending file:', err);
-            } else {
-                console.log('Sent:', filename);
-            }
-        });
+        res.sendFile(pathToFile);
         return
     } else {
         logger.info(pathToFile + " not exists")
